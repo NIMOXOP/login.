0d24276e2226f3601bb7ae886dd0cf5ae312c3b1
Shobhit Prajapati <117559866+NIMOXOP@users.noreply.github.com>
2024-08-05 00:54:29 +0530

    Add files via upload

---
+++ submitform.js
@@ -0,0 +1,11 @@
+function submitform(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    const responseDiv = document.getElementById('reponse').value;
+    responceDiv.innerHTML = <strong>Name:</strong>; $; { name; } <br>
+        <strong>name:</strong> ${name}<br>
+
+}</></>;
+}
